9939c36093eff1d39230c9af6dca8afc63c22ff3
andubiel <tdubiel@yahoo.com>
2026-07-01 11:18:19 -0400

    init

---
+++ netbox-docker/configuration/plugins.py
@@ -0,0 +1,15 @@
+PLUGINS = ["netbox_branching"]
+# Add your plugins and plugin settings here.
+# Of course uncomment this file out.
+
+# To learn how to build images with your required plugins
+# See https://github.com/netbox-community/netbox-docker/wiki/Using-Netbox-Plugins
+
+# PLUGINS = ["netbox_bgp"]
+
+# PLUGINS_CONFIG = {
+#   "netbox_bgp": {
+#     ADD YOUR SETTINGS HERE
+#   }
+# }
+
